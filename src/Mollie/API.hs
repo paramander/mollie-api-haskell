5446f8b3db5dd1c28249e1390048af9bc3d7aff0
Mats Rietdijk <matsrietdijk@gmail.com>
2016-08-16 10:18:40 +0200

    Add openssl with cacert validation

--- src/Mollie/API.hs
+++ src/Mollie/API.hs
@@ -6,11 +6,13 @@ module Mollie.API
     , Reader.liftIO
     ) where
 
-import qualified Control.Monad.Reader    as Reader
-import qualified Data.Text               as Text
+import qualified Control.Monad.Reader        as Reader
+import qualified Data.Text                   as Text
 import           Mollie.API.Internal
-import qualified Network.HTTP.Client     as HTTP
-import qualified Network.HTTP.Client.TLS as HTTP
+import qualified Network.HTTP.Client         as HTTP
+import qualified Network.HTTP.Client.OpenSSL as HTTP
+import qualified OpenSSL.Session             as OpenSSL
+import qualified Paths_mollie_api_haskell    as Self
 
 {-|
   Create a new Env from a Mollie API key.
@@ -18,8 +20,16 @@ import qualified Network.HTTP.Client.TLS as HTTP
   This key should start with either `test_` or `live_`.
 -}
 createEnv :: Text.Text -> IO Env
-createEnv key = do
-    manager <- HTTP.newManager HTTP.tlsManagerSettings
+createEnv key = HTTP.withOpenSSL $ do
+    sslContext <- OpenSSL.context
+    OpenSSL.contextSetVerificationMode sslContext OpenSSL.VerifyPeer
+        { OpenSSL.vpFailIfNoPeerCert = True
+        , OpenSSL.vpClientOnce       = False
+        , OpenSSL.vpCallback         = Nothing
+        }
+    cacert <- Self.getDataFileName "data/cacert.pem"
+    OpenSSL.contextSetCAFile sslContext cacert
+    manager <- HTTP.newManager . HTTP.opensslManagerSettings $ return sslContext
     return Env
         { env_key     = key
         , env_manager = manager
