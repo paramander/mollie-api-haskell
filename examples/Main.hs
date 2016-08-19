042a5bdfc4071bafd6a768c79f68722a137669ea
Mats Rietdijk <matsrietdijk@gmail.com>
2016-08-19 13:07:54 +0200

    Add ideal payment example

--- examples/Main.hs
+++ examples/Main.hs
@@ -1,9 +1,10 @@
+{-# LANGUAGE FlexibleContexts  #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
 import           Control.Monad.Reader
-import           Data.Text
+import           Data.Text            as Text
 import           Data.Text.Encoding
 import qualified Data.Text.Lazy       as TL
 import           Mollie.API
@@ -21,6 +22,10 @@ import qualified Data.HashMap.Strict  as HM
 import           System.Directory
 -- return page
 import           Mollie.API.Types
+-- ideal payment
+import           Lucid.Base
+import           Lucid.Html5
+import           Mollie.API.Issuers
 
 type Handler a = ActionT TL.Text (ReaderT Env IO) a
 
@@ -33,6 +38,8 @@ main = do
         get "/new-payment" newPaymentHandler
         post "/webhook-verification" webhookVerificationHandler
         get "/return-page" returnPageHandler
+        get "/ideal-payment" getIdealPaymentHandler
+        post "/ideal-payment" postIdealPaymentHandler
         notFound (text "Page not found")
 
 withMollie :: Mollie a -> Handler a
@@ -113,3 +120,63 @@ returnPageHandler = do
             paymentStatus <- liftIO $ fmap read $ readFile filepath :: Handler PaymentStatus
             text $ "Payment status for your order: " <> (TL.fromStrict $ toText paymentStatus)
         else text "Order not found."
+
+getIdealPaymentHandler :: Handler ()
+getIdealPaymentHandler = do
+    -- Get the first 250 issuers (there will probably be less).
+    -- In production you would want to check if there are more.
+    i <- withMollie $ getIssuers 0 250
+    case i of
+        Right issuersList -> do
+            -- Extract all ideal issuers for the list and display them in a form.
+            let issuers = list_data issuersList
+                idealIssuers = Prelude.filter ((==) Ideal . issuer_method) issuers
+                optionTag :: Issuer -> Html ()
+                optionTag issuer = option_
+                    [ value_ (issuer_id issuer) ]
+                    (toHtml $ issuer_name issuer)
+
+            html $ renderText $ do
+                form_ [ method_ "post" ] $ do
+                    label_ "Select your bank:"
+                    select_ [ name_ "issuer" ] $ do
+                        mapM_ optionTag idealIssuers
+                        option_ [ value_ "" ] "or select later"
+                    button_ "OK"
+        Left err -> raise $ TL.fromStrict $ "API call failed: " <> (pack $ show err)
+
+postIdealPaymentHandler :: Handler ()
+postIdealPaymentHandler = do
+    -- Get the selected issuer and check it for being empty.
+    -- In production your should check if this value is actually
+    -- a possible issuer for iDEAL, always check user input!
+    i <- param "issuer"
+    let issuer = if Text.null i then Nothing else Just i
+
+    -- Generate a unique identifier.
+    orderId <- fmap UUID.toText $ liftIO $ UUID.nextRandom
+
+    -- Determine the url for these examples.
+    hostUrl <- fmap (decodeUtf8 . guessApproot) $ request
+
+    -- Create the actual payment in Mollie.
+    p <- withMollie $ createPayment
+        (newPayment 27.50 "My first iDEAL payment" (hostUrl <> "/return-page?order_id=" <> orderId))
+        { newPayment_webhookUrl = Just (hostUrl <> "/webhook-verification")
+        , newPayment_metadata   = Just $ object ["order_id" .= orderId]
+        , newPayment_method     = Just Ideal
+        , newPayment_issuer     = issuer
+        }
+
+    case p of
+        Right payment -> do
+            -- Write the status to a file. In production you should probably use a
+            -- database.
+            liftIO $ writeFile (unpack $ "./orders/order-" <> orderId <> ".txt") (show $ payment_status payment)
+
+            -- This payment should always have a redirect url as we did not create
+            -- a recurring payment. So we can savely pattern match on a `Just` and
+            -- redirect the user to the checkout screen.
+            let Just redirectUrl = paymentLinks_paymentUrl $ payment_links payment
+            redirect $ TL.fromStrict redirectUrl
+        Left err -> raise $ TL.fromStrict $ "API call failed: " <> (pack $ show err)
