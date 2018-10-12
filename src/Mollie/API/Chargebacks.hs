949934dfa9a02d7fc4e6002d2afe999cd9b92904
Tolga Paksoy <tolga@paramander.com>
2018-10-12 13:09:33 +0200

    Add JSON type for Chargebacks

--- src/Mollie/API/Chargebacks.hs
+++ src/Mollie/API/Chargebacks.hs
@@ -16,9 +16,11 @@ module Mollie.API.Chargebacks
     , paymentId
     ) where
 
-import           Control.Lens     (makeFieldsNoPrefix)
-import qualified Data.Text        as Text
-import qualified Data.Time        as Time
+import           Control.Lens       (makeFieldsNoPrefix)
+import qualified Data.Aeson         as Aeson
+import qualified Data.Aeson.TH      as Aeson
+import qualified Data.Text          as Text
+import qualified Data.Time          as Time
 import           Mollie.API.Types
 
 data Chargeback = Chargeback
@@ -26,14 +28,20 @@ data Chargeback = Chargeback
     -- ^Mollies reference to the chargeback.
     , _amount           :: Amount
     -- ^The amount charged back by the consumer.
-    , _settlementAmount :: Amount
+    , _settlementAmount :: Maybe Amount
     -- ^The amount that will be deducted from your account
     , _createdAt        :: Time.UTCTime
     -- ^The date and time the chargeback was issued.
     , _reversedAt       :: Maybe Time.UTCTime
-    -- ^The date and time the chargeback was issued.
+    -- ^The date and time the chargeback was reversed.
     , _paymentId        :: Text.Text
     -- ^The unique identifier of the payment this chargeback was issued for.
     }
+
+$(Aeson.deriveFromJSON
+     Aeson.defaultOptions
+        { Aeson.fieldLabelModifier = drop 1
+        }
+     ''Chargeback)
 
 makeFieldsNoPrefix ''Chargeback
